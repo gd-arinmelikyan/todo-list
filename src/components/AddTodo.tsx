ff2fd4ae46ab4143368393a77a309e8c8b6ad4d6
Arin Melikyan <amelikyan@griddynamics.com>
2024-01-22 11:20:41 +0400

    minor changes

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,27 +1,35 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import './AddTodo.css';
 
-type NewItemProps = {
+interface NewItemProps {
   onAddNewItem: (todoText: string) => void;
 };
 
 const AddTodo: React.FC<NewItemProps> = ({ onAddNewItem }) => {
   const textInputRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
+
+    if (!enteredText.trim()) {
+      setError('Please enter a valid text.');
+      return;
+    }
+    setError(null);
     onAddNewItem(enteredText);
   };
 
   return (
     <form onSubmit={todoSubmitHandler}>
       <div className="form-control">
-        <label htmlFor="todo-text">text</label>
+        <label htmlFor="todo-text">ADD NEW TASK</label>
         <input type="text" id="todo-text" ref={textInputRef} />
+        {error && <div className="error-message">{error}</div>}
       </div>
-      <button type="submit">ADD NEW TASK</button>
+      <button type="submit">SUBMIT</button>
     </form>
   );
 };
